2c064f17f400aa4a4007b87c396fa376da453f97
Adel Rodríguez <adelrodriguezv@gmail.com>
2017-12-10 13:24:28 -0400

    Install packages and setup middleware

--- server/app.js
+++ server/app.js
@@ -1,8 +1,47 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const express = require('express');
+const helmet = require('helmet');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 
-// Express setup
+// DATABASE SETUP
+const mongoDB = process.env.DATABASE || 'mongodb://localhost/podcast-search';
+
+mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
+
+const database = mongoose.connection;
+
+database.on('error', console.error.bind(console, 'MongoDB connectior error:'));
+database.once('open', () => {
+  console.log('Connected to the database');
+});
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+// APP SETUP
 const app = express();
 
+// Express middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: err.message });
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'Error finding what you asked' });
+});
+
+// PORT SETUP
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
