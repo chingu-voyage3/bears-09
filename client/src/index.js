27ae4a3235ccaa0e22166ec5f1e7f6fd245241d9
Gustavo Azevedo <gustavo_azevedo_7@hotmail.com>
2017-12-11 23:51:55 -0200

    Add redux store and promise

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(rootReducer)} >
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
