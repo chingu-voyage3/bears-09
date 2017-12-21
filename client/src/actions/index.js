89e0c691948a841a4cdea16460f9fd4ebfa409c4
Chris Mayer <mayerxc@gmail.com>
2017-12-21 09:07:19 -0500

    add in redux for results container

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_PODCASTS = 'FETCH_PODCASTS'
 
 export function fetchPodcasts(term) {
   const req = axios.get(`https://itunes.apple.com/search?media=podcast&term=${term}`)
-
+  console.log("request is: ", req);
   return {
     type: FETCH_PODCASTS,
     payload: req
